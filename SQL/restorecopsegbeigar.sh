9f5f411305e0264b9fb592dc88b3c11f5a421611
UsuarioJoseLuis <UsuarioJoseLuis@192.168.0.12>
2021-05-06 16:12:07 +0200

    Modulo Administracion. Realiza correctamente la copia de seguridad

---
+++ SQL/restorecopsegbeigar.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+echo "Ejecutamos restaurar la copia de seguridad"
+
+
+var=$(date +"%FORMAT_STRING")
+now=$(date +"%m_%d_%Y")
+printf "%s\n" $now
+today=$(date +"%Y_%m_%d")
+
+echo today
+
+/Applications/XAMPP/bin/mysql -u root -proot19 < fichero.sql
